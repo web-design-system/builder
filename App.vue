645999e87cece3ff3cf69740c53a34e74c5d649f
Darlan Alves <me@darlanalv.es>
2023-11-08 06:48:06 +0530

    feat: use custom CSS

--- App.vue
+++ App.vue
@@ -1,12 +1,20 @@
 <template>
-  <div class="flex flex-col w-screen h-screen p-4">
-    <div class="md:grid-columns-2 flex-grow flex gap-2 bg-gray-100">
-      <code class="whitespace-pre h-1/2 overflow-auto bg-white rounded shadow">{{ snippet }}</code>
-      <div class="flex-grow bg-white rounded shadow" v-html="snippet"></div>
+  <div class="builder">
+    <div class="builder__canvas">
+      <code
+        class="builder__code"
+        >{{ snippet }}</code
+      >
+      <div class="builder__preview" v-html="snippet"></div>
     </div>
-    <div class="flex p-4">
-      <textarea :rows="height" aut class="flex-grow p-2" v-model="input" @keyup="onKeyUp"></textarea>
-      <button class="px-4 py-2 bg-blue-500 text-white" @click="apply">
+    <div class="builder__instruction">
+      <textarea
+        :rows="height"
+        class="builder__input"
+        v-model="input"
+        @keyup="onKeyUp"
+      ></textarea>
+      <button class="builder__btn" @click="apply">
         <span v-if="running" class="material-icons animate-spin">refresh</span>
         <span v-else class="material-icons">send</span>
       </button>
@@ -17,37 +25,45 @@
 <script setup lang="ts">
 import { computed, ref } from "vue";
 import buildHtmlComponent from "https://aifn.run/fn/fffeebb3-4605-4b91-ad56-7c25a2344b3c.js";
+import generateHtmlFromInput from "https://aifn.run/fn/08d5dd80-9dc2-4ca2-9354-b16433986b16.js";
 
 const input = ref("");
 const snippet = ref("");
 const running = ref(false);
-const height = computed(() => Math.min(input.value.split('\n').length + 1, 1));
+const height = computed(() => Math.min(input.value.split("\n").length + 1, 1));
 
 async function apply() {
   running.value = true;
+  let code = snippet.value;
 
-  let code = await buildHtmlComponent({
-    snippet: snippet.value,
-    input: input.value,
-  });
+  if (code) {
+    code = await buildHtmlComponent({
+      snippet: code,
+      input: input.value,
+    });
+  } else {
+    code = await generateHtmlFromInput({ input: input.value });
+  }
 
   if (code) {
     snippet.value = sanitize(code);
-    input.value = "";
   }
 
+  input.value = "";
   running.value = false;
 }
 
 function sanitize(code) {
-  if (code.trim().includes('```')) {
-    return code.replaceAll('```html', '').replaceAll('```', '');
+  if (code.trim().includes("```")) {
+    return code.replaceAll("```html", "").replaceAll("```", "");
   }
 
   return code;
 }
 
 function onKeyUp(event: KeyboardEvent) {
-  event.key === "Enter" && apply();
+  if (event.key === "Enter" && (event.ctrlKey || event.metaKey)) {
+    apply();
+  }
 }
 </script>
